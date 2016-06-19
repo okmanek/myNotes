8d90af485c5c4b1be6ac8b63e08eb7ce0dd41dcb
okmanek <okmanek@onet.eu>
2016-06-19 17:09:27 +0200

    added prepending to a file and showing file content when no parameters

--- note.py
+++ note.py
@@ -3,12 +3,33 @@ import sys
 
 argc = len(sys.argv)
 
+notePath = '/home/i/iop/note.txt'
+notePath2 = '/home/i/iop/note.txt'
+
+#http://stackoverflow.com/questions/5914627/prepend-line-to-beginning-of-a-file
+def prepend_line(filename, line):
+    with open(filename, 'r+') as f:
+        content = f.read()
+        f.seek(0, 0)
+        f.write(line.rstrip('\r\n') + '\n' + content)
+
 if(argc == 1):
-  print "nie podano dodatkowych argumentow"
+  file = open(notePath, 'r')
+  fileContent = file.read()
+  print fileContent
+  file.close()
 else:
   if(sys.argv[1] == "+"):
-    print "dodaj"
+    print "dodaje"
     #kod dodania do note.txt
+    #otworz plik z notes
+    #file = open(notePath, 'w')
+    #open(notePath, "w").write("#test firstline\n" + open(notePath).read())
+    prepend_line(notePath, "");#bcs we want an empty line between lines
+    prepend_line(notePath, sys.argv[2])
+    print 'dodano'
+    #
+    #
 
 
 #print sys.argv
