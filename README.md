8799ace06df6ed5d8c3a29cbf2e2c2e964effd25
okmanek <okmanek@onet.eu>
2016-07-24 12:56:37 +0200

    minor fix : )

--- README.md
+++ README.md
@@ -1,17 +1,30 @@
 myNotes is my attempt to create terminal-based notes tool.
 I'll start with using aliases and simple python script.
 
+motto of myNotes is:
+"OSZCZĘDZAJ RAM GDZIEKOLWIEK JESTEŚ"
+
 To make it work you need to add an alias:
 >alias note ~/note.py
 where ~/note.py is path to python script
+alias note ~/code/github-okmanek/myNotes/note.py
 
 Example of usage:
->note + "dupa"
-adds note "dupa" to certain file.
+  >note + "dupa"
+  adds note "dupa" to certain file.
+
+Arguments:
+  p|a - prepends|appends text to the file
+  P|A - removes first|last text in the file
+  s - searches for a text and returns all occurences
 
 toDo functionalities:
->seeing notes
->removing notes
->backup - uploading to the server
->synchronising
->many platforms
+  >seeing notes
+  >removing notes
+  >backup - uploading to the server
+  >synchronising
+  >many platforms
+  >Firefox extension adding selected text to the file
+  >dictionary mapping zamiast tylu ifów
+    http://stackoverflow.com/questions/60208/replacements-for-switch-statement-in-python
+    http://www.pydanny.com/why-doesnt-python-have-switch-case.html
--- note.py
+++ note.py
@@ -1,36 +1,82 @@
 #!/usr/bin/python
-import sys
+from sys import argv
 
-argc = len(sys.argv)
+argc = len(argv)
+availableOperators = ('+', '-', '/')
+#+ dodawanie, - odejmowanie, / szukanie
 
-notePath = '/home/i/iop/note.txt'
-notePath2 = '/home/i/iop/note.txt'
+#filepaths:
+#tak naprawde nie bedzie potrzebne (chyba)
+notePath = '/home/i/.myNotes/note.txt'
+musePath = '/home/i/.myNotes/muse.txt'
+filmPath = '/home/i/.myNotes/film.txt'
+bookPath = '/home/i/.myNotes/book.txt'
 
 #http://stackoverflow.com/questions/5914627/prepend-line-to-beginning-of-a-file
-def prepend_line(filename, line):
+def prependLine(filename, line):
     with open(filename, 'r+') as f:
         content = f.read()
         f.seek(0, 0)
         f.write(line.rstrip('\r\n') + '\n' + content)
 
-if(argc == 1):
-  file = open(notePath, 'r')
-  fileContent = file.read()
-  print fileContent
-  file.close()
-else:
-  if(sys.argv[1] == "+"):
-    print "dodaje"
-    #kod dodania do note.txt
-    #otworz plik z notes
-    #file = open(notePath, 'w')
-    #open(notePath, "w").write("#test firstline\n" + open(notePath).read())
-    prepend_line(notePath, "");#bcs we want an empty line between lines
-    prepend_line(notePath, sys.argv[2])
-    print 'dodano'
-    #
-    #
 
 
-#print sys.argv
-#print sys.argv
+
+if( argc < 3 ):#too few args
+	print 'za malo argumentow'
+
+elif(argc == 3):#example: ">note dupa"
+	#choosing right folder based on parameter from alias
+	if( argv[1] == 'note' ):
+		filePath = notePath
+		print notePath
+	elif( argv[1] == 'muse' ):
+		filePath = musePath
+	elif( argv[1] == 'film' ):
+		filePath = filmPath
+	elif( argv[1] == 'book' ):
+		filePath = bookPath
+
+elif( argc == 4):#example: ">note + dupa"
+	if( argv[2] == '+' ):#dodanie nowego wpisu
+		#adding note to a file
+		prependLine(notePath, "")
+		prependLine( notePath, argv[3] )
+		print 'added text: ' + argv[3]
+	print '4'
+
+
+	#print 'przy wyborze pliku: zly parametr'
+	#nie bedzie przypadku podania zlego parametru bo aliasy mapuja bezposrednio do tylko 4 opcji
+
+	# 
+	# 	print '+'
+	# elif( argv[2] == '-' ):#usuniecie wpisu
+	# 	print '- not handled yet'
+	# elif( argv[2] == '/' ) or ( argv[2] == '?' ):
+	# 	print 'searching not handled yet'
+
+
+# if(sys.argv[1] == "song"):
+#   print "SONG"
+
+# if(argc == 1):
+#   file = open(notePath, 'r')
+#   fileContent = file.read()
+#   print fileContent
+#   file.close()
+# else:
+#   if(sys.argv[1] == "+"):
+#     #kod dodania do note.txt
+#     #otworz plik z notes
+#     #file = open(notePath, 'w')
+#     #open(notePath, "w").write("#test firstline\n" + open(notePath).read())
+#     prepend_line(notePath, "");#bcs we want an empty line between lines
+#     prepend_line(notePath, sys.argv[2])
+#     print 'added text: ' + sys.argv[2]
+#     #
+#     #
+
+
+# #print sys.argv
+# #print sys.argv
