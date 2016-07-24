0541d252a7ef9bb110667205de4a520542ccc3ad
okmanek <okmanek@onet.eu>
2016-07-24 14:25:55 +0200

    bug: adding note deletes file before adding

--- note.py
+++ note.py
@@ -6,7 +6,7 @@ availableOperators = ('+', '-', '/', '?')#needed?
 
 filePaths = {
 	'note' : '/home/i/.myNotes/note.txt',
-	'muse' : '/home/i/.myNotes/muse.txt',
+	'song' : '/home/i/.myNotes/song.txt',
 	'film' : '/home/i/.myNotes/film.txt',
 	'book' : '/home/i/.myNotes/book.txt',
 }
@@ -18,32 +18,39 @@ def prependLine(filename, line):
         f.seek(0, 0)
         f.write(line.rstrip('\r\n') + '\n' + content)
 
-if( argc < 3 ):#too few args
+if( argc < 2 ):#too few args
 	print 'za malo argumentow'
 
-elif(argc == 3):#example: ">note dupa"
-	#choosing right folder based on parameter from alias
+else:#if number of args is 2 or more, choose appropriate filepath
 	if( argv[1] == 'note' ):
 		filePath = filePaths['note']
-		print filePath
-	elif( argv[1] == 'muse' ):
-		filePath = filePaths['muse']
+	elif( argv[1] == 'song' ):
+		filePath = filePaths['song']
 	elif( argv[1] == 'film' ):
 		filePath = filePaths['film']
 	elif( argv[1] == 'book' ):
 		filePath = filePaths['book']
+		#note that there cannot be bad parameter because of the aliases system
+	
+	if( argc == 2 ):#just show content
+		file = open(filePath, 'r')
+		fileContent = file.read()
+		print fileContent
+		file.close()
 
-elif( argc == 4):#example: ">note + dupa"
-	if( argv[2] == '+' ):#dodanie nowego wpisu
-		#adding note to a file
-		prependLine(notePath, "")
-		prependLine( notePath, argv[3] )
-		print 'added text: ' + argv[3]
-	print '4'
+	if( argc == 3 ):#just for now
+		print argv[2]
+		file = open(filePath, 'w')
+		prependLine(filePath, "")
+		prependLine( filePath, argv[2] )
+		#open(filePath, "w").write("#test firstline\n" + open(filePath).read())
+		print 'added text: ' + argv[2]
+		file.close()
+		print 'added'
+	
+	if( argc == 4):
+		pass
 
-
-	#print 'przy wyborze pliku: zly parametr'
-	#nie bedzie przypadku podania zlego parametru bo aliasy mapuja bezposrednio do tylko 4 opcji
 
 	# 
 	# 	print '+'
@@ -53,26 +60,13 @@ elif( argc == 4):#example: ">note + dupa"
 	# 	print 'searching not handled yet'
 
 
-# if(sys.argv[1] == "song"):
-#   print "SONG"
-
-# if(argc == 1):
-#   file = open(notePath, 'r')
-#   fileContent = file.read()
-#   print fileContent
-#   file.close()
 # else:
 #   if(sys.argv[1] == "+"):
 #     #kod dodania do note.txt
 #     #otworz plik z notes
-#     #file = open(notePath, 'w')
 #     #open(notePath, "w").write("#test firstline\n" + open(notePath).read())
 #     prepend_line(notePath, "");#bcs we want an empty line between lines
 #     prepend_line(notePath, sys.argv[2])
 #     print 'added text: ' + sys.argv[2]
 #     #
 #     #
-
-
-# #print sys.argv
-# #print sys.argv
