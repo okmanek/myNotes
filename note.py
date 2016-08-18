6e59aa4855b13ba62121a321495f36367ffd78f7
okmanek <okmanek@onet.eu>
2016-08-18 21:38:56 +0200

    code completely restructured. many words without double quotes now can be noted. available operators now in list.

--- note.py
+++ note.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python
+############ imports ############
 from sys import argv
 import os
+import argparse
 
+############ variables ############
 argc = len(argv)
 availableOperators = ('+', '-', '/', '?')#needed?
 searchResultNeighbours = 1 #how many surrounding lines is shown while searching
+opers = [ '+', '/', '?' ]#available operators
 
 filePaths = {
   'note' : '/home/i/.myNotes/note.txt',
@@ -13,6 +17,7 @@ filePaths = {
   'book' : '/home/i/.myNotes/book.txt',
 }
 
+############ functions ############
 #http://stackoverflow.com/questions/5914627/prepend-line-to-beginning-of-a-file
 def prependLine(filename, line):
     with open(filename, 'r+') as f:
@@ -20,7 +25,14 @@ def prependLine(filename, line):
         f.seek(0, 0)
         f.write(line.rstrip('\r\n') + '\n' + content)
 
-if( argc < 2 ):#too few args
+def showContent():
+  file = open(filePath, 'r')
+  fileContent = file.read()
+  print fileContent
+  file.close()
+
+############ code itself ############
+if( argc < 2 ):#only happens when you execute script without using alias
   print 'za malo argumentow'
 
 else:#if number of args is 2 or more, choose appropriate filepath
@@ -31,17 +43,29 @@ else:#if number of args is 2 or more, choose appropriate filepath
   #> note
   #just show content  
   if( argc == 2 ):#just show content
-    file = open(filePath, 'r')
-    fileContent = file.read()
-    print fileContent
-    file.close()
-
-  #>note "text to add"
-  if( argc == 3 ):#just for now
-    prependLine(filePath, "")
-    prependLine( filePath, argv[2] )
-    print 'added text: ' + argv[2]
-
+    showContent()
+  else:
+    if(argc == 3):#>note arg1
+      if argv[2] in opers:#>note +
+        print 'blad, podales operator bez zadnych danych'
+        #so you cannot note for example plus sign. I doubt it is a problem though
+      else:#note dupa
+        prependLine(filePath, "")
+        prependLine( filePath, argv[2] )#file opened twice. TODO: fix it
+        print 'added text: ' + argv[2]
+    else:#>note arg1 arg2 (...)
+      if argv in opers:#>note + "text to process"
+        pass
+        #zapisanie do pliku argv[2:] (chyba)
+      else:#>note dupa1 dupa2 dupa3
+        #pass
+        textToSave = " ".join(argv[2:])
+        prependLine(filePath, "")
+        prependLine( filePath, textToSave )#file opened twice. TODO: fix it
+        print 'added text: ' + textToSave
+        #zapisanie do pliku argv[1:] (chyba)
+  
+"""
   #>note + "text to process"
   # where '+' can be any operator
   if( argc == 4):
@@ -62,4 +86,4 @@ else:#if number of args is 2 or more, choose appropriate filepath
       with open("a.txt", "w") as textObj:
         for n in list:
           textObj.write(n)
-
+"""
