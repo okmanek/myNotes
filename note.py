6b6b302f4a68668de3544267ca219f9afa721437
okmanek <okmanek@onet.eu>
2016-08-18 22:22:52 +0200

    added copyright info. deleted some trash commented text

--- note.py
+++ note.py
@@ -1,8 +1,24 @@
 #!/usr/bin/python
+
+#----------------------------------------------------------------------------
+#"THE BEER-WARE LICENSE" (Revision 42):
+#<phk@FreeBSD.ORG> wrote this file.  As long as you retain this notice you
+#can do whatever you want with this stuff. If we meet some day, and you think
+#this stuff is worth it, you can buy me a beer in return.   okmanek
+#----------------------------------------------------------------------------
+
+#https://github.com/okmanek/myNotes
+
 ############ imports ############
 from sys import argv
 import os
 import argparse
+
+#TODO: all in 1 class
+
+#arguments:
+#>note
+#['/home/i/code/github-okmanek/myNotes/note.py', 'book']
 
 ############ variables ############
 argc = len(argv)
